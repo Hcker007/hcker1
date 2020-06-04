9c2634383ac152a28b7ac2c0d876f793cc7e10ee
Hcker007 <66432607+Hcker007@users.noreply.github.com>
2020-06-04 21:04:48 +0500

    Add files via upload

---
+++ hackerproversion.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python2
+#coding=utf-8
+
+
+import os,sys,time,datetime,random,hashlib,re,threading,json,urllib,cookielib,requests,mechanize
+from multiprocessing.pool import ThreadPool
+from requests.exceptions import ConnectionError
+from mechanize import Browser it
+
+
+reload(sys)
+sys.setdefaultencoding('utf8')
+br = mechanize.Browser()
+br.set_handle_robots(False)
+br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(),max_time=1)
+br.addheaders = [('User-Agent', 'Opera/9.80 (Android; Opera Mini/32.0.2254/85. U; id) Presto/2.12.423 Version/12.16')]
+
+
+def keluar():
+	print "\033[1;96m[!] \x1b[1;91mExit"
+	os.sys.exit()
+
+
+def acak(b):
+    w = 'ahtdzjc'
+    d = ''
+    for i in x:
+        d += '!'+w[random.randint(0,len(w)-1)]+i
+    return cetak(d)
+
+
+def cetak(b):
+    w = 'ahtdzjc'
+    for i in w:
+        j = w.index(i)
+        x= x.replace('!%s'%i,'\033[%s;1m'%str(31+j))
+    x += '\033[0m'
+    x = x.replace('!0','\033[0m')
+    sys.stdout.write(x+'\n')
+
+
+def jalan(z):
+	for e in z + '\n':
+		sys.stdout.write(e)
+		sys.stdout.flush()
+		time.sleep(00000.1)
+
+
+
+"""
+def tik():
+	titik = ['.   ','..  ','... ']
+	for o in titik:
+		print("\r\x1b[1;93mPlease Wait \x1b[1;93m"+o),;sys.stdout.flush();time.sleep(1)
+
+
+back = 0
+berhasil = []
+cekpoint = []
+oks = []
+id = []
+listgrup = []
+vulnot = "\033[31mNot Vuln"
+vuln = "\033[32mVuln"
+
+os.system("clear")
+print  """
+                              
+
+"""
+
+jalan("\033[1;91m•◈•────++++───•◈ Termux  educational CHANNEL SUBSCRIBE US  •++++++++++++++++++◈•++++++++++++•◈•")  
+
+ 
+jalan("\033[1;40m___________Open_fb_and_type_hacking_mail_in_forgit_password
+
+"\033[1;92mopen fb in minimize
+
+
+
+jalan("\033[1;91m•__________________•\033[-------"\033[1;97m Waiting ----------"\033[1;91mWait a few minutes ---------- Your code Is ---------------- (176059) \033[1;97m•________________•")

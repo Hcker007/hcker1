0cabf70941f5310a6a8f62f839c17043fb705719
Hcker007 <66432607+Hcker007@users.noreply.github.com>
2020-06-04 21:10:57 +0500

    Add files via upload

--- hackerproversion.py
+++ hackerproversion.py
@@ -1,10 +1,1 @@
-jalan("\033[1;91m•◈•────++++───•◈ Termux  educational CHANNEL SUBSCRIBE US  •++++++++++++++++++◈•++++++++++++•◈•")  
-
- 
-jalan("\033[1;40m___________Open_fb_and_type_hacking_mail_in_forgit_password
-
-"\033[1;92mopen fb in minimize
-
-
-
 jalan("\033[1;91m•__________________•\033[-------"\033[1;97m Waiting ----------"\033[1;91mWait a few minutes ---------- Your code Is ---------------- (176059) \033[1;97m•________________•")
